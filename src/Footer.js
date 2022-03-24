b8b00afdd578b5169c86a3ae7736b94b089ed0ea
iva-pa <94801213+iva-pa@users.noreply.github.com>
2022-03-24 20:27:15 +0100

    Updated link to GitHub repository

--- src/Footer.js
+++ src/Footer.js
@@ -7,7 +7,7 @@ export default function Footer() {
         This project was coded by Iva Papcunova and is{" "}
       </span>
       <a
-        href="https://github.com/iva-pa/react-weather-app"
+        href="https://github.com/iva-pa/weather-react-app"
         target="_blank"
         rel="noopener noreferrer"
         id="github-link"
